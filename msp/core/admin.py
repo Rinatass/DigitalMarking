6f8a5dbbf2d025c0403abf95b24c189d10d1bcd0
Rinat <oblom.ru@gmail.com>
2025-05-12 22:13:21 +0300

    add product model

--- msp/core/admin.py
+++ msp/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    list_filter = ('owner',)
+    search_fields = ('name', 'owner__username')
+    exclude = ('owner',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+    def save_model(self, request, obj, form, change):
+        if not change or not obj.owner_id:
+            obj.owner = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Product, ProductAdmin)
--- msp/core/models.py
+++ msp/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return f'{self.name} ({self.owner.username})'
